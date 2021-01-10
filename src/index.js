811a3629a4b836ea9a2ed232345dc6b78cc53546
Jaskaran Singh <42887065+JaskaranSingh4704@users.noreply.github.com>
2021-01-10 11:29:21 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: "#app-4",
+  data: {
+    todos: [
+      { text: "HTML Tutorial" },
+      { text: "CSS Tutorial" },
+      { text: "JavaScript Tutorial" },
+      { text: "AngularJS Tutorial" },
+      { text: "Vue.js Tutorial" }
+    ]
+  }
+});
