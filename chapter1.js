6f113947e8bf69a77920ae4e10d43425fdce7a5f
Jaskaran Singh <jaskaransingh4704@gmail.com>
2021-01-11 17:47:03 +0530

    attribute bindings

--- chapter1.js
+++ chapter1.js
@@ -1,15 +1,24 @@
 new Vue({
   el: "#chapter2",
   data: {
+    buttonTitle: "this is javascript button title",
     newName: "",
     names: ["jas", "simran", "manu"],
+    myClass: "",
+    toggle :true,
   },
 
   methods: {
     addName() {
-        if(this.newName !== ""){
-            this.names.push(this.newName)
-        }
+      if (this.newName !== "") {
+        this.names.push(this.newName);
+      }
     },
+    changeClass() {
+      this.myClass = "myClassName";
+    },
+    togglePeragraph(){
+        this.toggle = !this.toggle
+    }
   },
 });
