60e472acbe97847523f157fea8afc54d9a4514a5
Jaskaran Singh <jaskaransingh4704@gmail.com>
2021-01-12 09:30:05 +0530

    create two components

--- vue-components/index.js
+++ vue-components/index.js
@@ -1,7 +1,48 @@
-Vue.component('task', {
-    template:`<li><slot></slot></li>`,
-})
+Vue.component("task-list", {
+  template: `
+  <div>
+    <task v-for="task in tasks" v-text="task.taskName" :key="task.id"></task>
+  </div>   
+  `,
+  data() {
+    return {
+      tasks: [
+        {
+          taskName: "Go To school",
+          isCompleted: true,
+        },
+        {
+          taskName: "Go To Office",
+          isCompleted: false,
+        },
+        {
+          taskName: "Go To Town",
+          isCompleted: true,
+        },
+        {
+          taskName: "Go To Work",
+          isCompleted: false,
+        },
+        {
+          taskName: "Go To heaven",
+          isCompleted: true,
+        },
+      ],
+    };
+  },
+  computed:{
+      completed(){
+        return this.tasks.filter((task)=>{
+            return task.isCompleted
+        })
+      }
+  }
+});
+
+Vue.component("task", {
+  template: `<li><slot></slot></li>`,
+});
 
 new Vue({
-    el:'#myApp',
-})
+  el: "#myApp",
+});
